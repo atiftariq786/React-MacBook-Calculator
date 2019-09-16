5c24e33aa6da33114d5dc20acb6bd397c6c079ff
Qandeel Sajid <atiftariq786@hotmail.com>
2019-09-16 11:58:37 -0700

    Component: code chunk added in inputDispaly component

--- macbookcalculator/src/components/Buttons/index.js
+++ macbookcalculator/src/components/Buttons/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./style.css";
 
-const Buttons = () => {
+const buttons = () => {
 
     return (
         <div>
@@ -10,4 +10,4 @@ const Buttons = () => {
     )
 }
 
-export default Buttons;
+export default buttons;
---
+++ macbookcalculator/src/components/InputDisplay/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+const inputDisplay = () => {
+
+    return (
+        <div>
+        <button>Click me!</button>
+        </div>
+    )
+}
+
+export default inputDisplay;
