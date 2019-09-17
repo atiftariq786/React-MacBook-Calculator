b93319699b388536ab7a5fb983bd57eefdc60a11
Qandeel Sajid <atiftariq786@hotmail.com>
2019-09-17 15:17:23 -0700

    Button styling update

--- macbookcalculator/src/components/Buttons/index.js
+++ macbookcalculator/src/components/Buttons/index.js
@@ -22,44 +22,38 @@ const buttons = (props) => {
     
         <Container className="container">
             <Row>              
-                <Col xs = "3"><button className="numButton">AC</button></Col> 
-                <Col xs ="3"><button className="numButton">+/-</button></Col> 
-                <Col xs = "3"><button className="numButton">%</button></Col> 
-                <Col xs = "3"><button className="numButton">/</button></Col>      
+                <Col lg = {true}><button className="numButton">AC</button></Col> 
+                <Col lg ="3"><button className="numButton">+/-</button></Col> 
+                <Col lg = "3"><button className="numButton">%</button></Col> 
+                <Col lg = "3"><button className="symButton">/</button></Col>      
         
             </Row>
             <Row>              
                 <Col><button className="numButton">7</button></Col> 
                 <Col><button className="numButton">8</button></Col> 
                 <Col><button className="numButton">9</button></Col> 
-                <Col><button className="numButton">*</button></Col>      
+                <Col><button className="symButton">*</button></Col>      
         
             </Row>
             <Row>              
                 <Col><button className="numButton">4</button></Col> 
                 <Col><button className="numButton">5</button></Col> 
                 <Col><button className="numButton">6</button></Col> 
-                <Col><button className="numButton">-</button></Col>      
+                <Col><button className="symButton">-</button></Col>      
         
             </Row>
-            <Row>              
-                <Col><button className="numButton">1</button></Col> 
-                <Col><button className="numButton">2</button></Col> 
-                <Col><button className="numButton">3</button></Col> 
-                <Col><button className="numButton">+</button></Col>      
-        
-            </Row>
+            
              <Row>              
                 <Col><button className="numButton">1</button></Col> 
                 <Col><button className="numButton">2</button></Col> 
                 <Col><button className="numButton">3</button></Col> 
-                <Col><button className="numButton">+</button></Col>      
+                <Col><button className="symButton">+</button></Col>      
         
             </Row>
             <Row>              
-                <Col md={4}><button className="numButton">0</button></Col>                
-                <Col ><button className="numButton">.</button></Col> 
-                <Col><button className="numButton">=</button></Col>      
+                <Col md={6}><button className="numButton">0</button></Col>                
+                <Col md={3}><button className="numButton">.</button></Col> 
+                <Col md={3}><button className="symButton">=</button></Col>      
     
             </Row>
 
