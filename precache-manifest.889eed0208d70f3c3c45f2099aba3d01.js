2b36de191daab9ce1e6f011972d31dd906b70a99
Qandeel Sajid <atiftariq786@hotmail.com>
2019-09-30 15:24:32 -0700

    Updates

---
+++ precache-manifest.889eed0208d70f3c3c45f2099aba3d01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a734c02498fdc043a22ba2e378e054b2",
+    "url": "/React-MacBook-Calculator/index.html"
+  },
+  {
+    "revision": "50e3bba48a2f3b3e5360",
+    "url": "/React-MacBook-Calculator/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "550837b16957aef54067",
+    "url": "/React-MacBook-Calculator/static/css/main.2750e613.chunk.css"
+  },
+  {
+    "revision": "50e3bba48a2f3b3e5360",
+    "url": "/React-MacBook-Calculator/static/js/2.16a1aa4a.chunk.js"
+  },
+  {
+    "revision": "550837b16957aef54067",
+    "url": "/React-MacBook-Calculator/static/js/main.328b09aa.chunk.js"
+  },
+  {
+    "revision": "e4f0ef4878749ce9da94",
+    "url": "/React-MacBook-Calculator/static/js/runtime~main.8041f7a3.js"
+  }
+]);
